0e299222074021918bb6eeb3dfd388905b889eb4
NOEL-code <junusong12@naver.com>
2024-07-07 02:48:44 +0900

    .gitignore 생성

--- app_back/src/controllers/userController.js
+++ app_back/src/controllers/userController.js
@@ -1,50 +1,21 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const { User } = require("../models/User");
+const { registerUser } = require("../services/userService");
 
 exports.registerUser = async (req, res) => {
   const { userEmail, userPassword, name, phoneNumber, photoUrl } = req.body;
 
   try {
-    // 이메일 중복 체크
-    let user = await User.findOne({ userEmail });
-    if (user) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    // 비밀번호 해싱
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(userPassword, salt);
-
-    // 새로운 사용자 생성
-    user = new User({
+    const token = await registerUser({
       userEmail,
-      userPassword: hashedPassword,
+      userPassword,
       name,
       phoneNumber,
       photoUrl,
     });
-
-    // 사용자 저장
-    await user.save();
-
-    // JWT 토큰 생성
-    const payload = {
-      user: {
-        id: user.id,
-      },
-    };
-
-    jwt.sign(
-      payload,
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" },
-      (err, token) => {
-        if (err) throw err;
-        res.status(201).json({ token });
-      }
-    );
+    res.status(201).json({ token });
   } catch (err) {
+    if (err.message === "User already exists") {
+      return res.status(400).json({ message: err.message });
+    }
     console.error(err.message);
     res.status(500).json({ message: "Server error" });
   }
