04f8362c5427edc37dc41e3d803624c2622165ca
NOEL-code <junusong12@naver.com>
2024-07-09 22:41:06 +0900

    빌드 다시함

--- app/src/Screen/MyPage/LogIn.js
+++ app/src/Screen/MyPage/LogIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -7,25 +7,21 @@ import {
   TextInput,
   Alert,
 } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
-import { setUser } from '../../redux/userSlice'; // Adjust the import path as needed
-import { fetchLogin } from '../../Service/user'; // Adjust the import path as needed
+import {useDispatch, useSelector} from 'react-redux';
+import {setUser} from '../../redux/userSlice'; // Adjust the import path as needed
+import {fetchLogin} from '../../Service/user'; // Adjust the import path as needed
 
-const LogIn = ({ navigation }) => {
+const LogIn = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const dispatch = useDispatch();
   const user = useSelector(state => state.user);
 
   const handleLogin = async () => {
-<<<<<<< HEAD
     try {
-      const result = await fetchLogin(email, password);
-=======
-    console.log('로그인 누름');
+      console.log('로그인 누름');
 
-    const result = await fetchLogin(email, password);
->>>>>>> b24421aec0d1e293e1b322ed875a045689100453
+      const result = await fetchLogin(email, password);
 
       if (!result || result.error || !result.id) {
         Alert.alert('로그인 실패', '아이디와 비밀번호를 확인해주세요.');
@@ -46,7 +42,10 @@ const LogIn = ({ navigation }) => {
       }
     } catch (error) {
       console.error('Login error:', error);
-      Alert.alert('로그인 실패', '서버 오류가 발생했습니다. 나중에 다시 시도해주세요.');
+      Alert.alert(
+        '로그인 실패',
+        '서버 오류가 발생했습니다. 나중에 다시 시도해주세요.',
+      );
     }
   };
 
